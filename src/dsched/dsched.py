03316f962581b7cb612c0804f561cd073af503a0
Ralph Castain <rhc@pmix.org>
2023-03-15 07:36:51 -0700

    Checkpoint start of allocation procedure
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/dsched/dsched.py
+++ src/dsched/dsched.py
@@ -10,9 +10,9 @@ import threading
 import socket
 
 # project imports
-import server_upcalls
 from fifo import *
-import pmix
+from pmix import *
+global killer
 
 terminate_event = threading.Event()
 
@@ -25,6 +25,7 @@ def handler(signum, frame):
 def main():
     global terminate_event
 
+    # get a scheduler object
     try:
         dsched = Scheduler()
     except Exception as error:
@@ -35,24 +36,19 @@ def main():
     # initialize the queues
     try:
         dsched.queue_init()
-        print("QUEUES INIT")
     except Exception as error:
         print("QUEUE INIT FAILED");
         exit(1)
 
-    try:
-        queues = dsched.queue_names()
-        print("QUEUES:", queues)
-    except Exception as error:
-        exit(1)
-
-    try:
-        dsched.query_allocation()
-    except Exception as error:
-        print("QUERY FAILED")
-        pass
-#        dsched.finalize()
-#        exit(1)
+    # if we attached to a DVM, then
+    # query it for the available resources
+    if self.dvmattached:
+        try:
+            dsched.query_allocation()
+        except Exception as error:
+            print("QUERY FAILED")
+            dsched.finalize()
+            exit(1)
 
     # setup the signal handler - we protect each
     # call in case a particular system doesn't
@@ -81,11 +77,12 @@ def main():
     dsched.finalize()
 
 
-class Scheduler(pmix.PMIxScheduler):
+class Scheduler(PMIxScheduler):
     def __init__(self):
         self.allocation = []
         self.topologies = []
         self.pid = os.getpid()
+        self.dvmattached = False
         try:
             self.hname = socket.gethostname()
         except:
@@ -93,57 +90,57 @@ class Scheduler(pmix.PMIxScheduler):
         self.fifo = None
         args = [
             {
-                "key": pmix.PMIX_TOOL_NSPACE,
+                "key": PMIX_TOOL_NSPACE,
                 "value": "DynaSched." + self.hname + '.' + str(self.pid),
-                "val_type": pmix.PMIX_STRING,
+                "val_type": PMIX_STRING,
             },
             {
-                "key": pmix.PMIX_TOOL_RANK,
+                "key": PMIX_TOOL_RANK,
                 "value": 0,
-                "val_type": pmix.PMIX_PROC_RANK
+                "val_type": PMIX_PROC_RANK
             },
             {
-                "key": pmix.PMIX_SERVER_SCHEDULER,
+                "key": PMIX_SERVER_SCHEDULER,
                 "value": "T",
-                "val_type": pmix.PMIX_BOOL
+                "val_type": PMIX_BOOL
             },
             {
-                "key": pmix.PMIX_CONNECT_TO_SYSTEM,
+                "key": PMIX_CONNECT_TO_SYSTEM,
                 "value": "T",
-                "val_type": pmix.PMIX_BOOL
+                "val_type": PMIX_BOOL
             },
             {
-                "key": pmix.PMIX_TOOL_CONNECT_OPTIONAL,
+                "key": PMIX_TOOL_CONNECT_OPTIONAL,
                 "value": "T",
-                "val_type": pmix.PMIX_BOOL
+                "val_type": PMIX_BOOL
             },
             {
-                "key": pmix.PMIX_SERVER_REMOTE_CONNECTIONS,
+                "key": PMIX_SERVER_REMOTE_CONNECTIONS,
                 "value": "T",
-                "val_type": pmix.PMIX_BOOL
+                "val_type": PMIX_BOOL
             },
         ]
         rc, self.myproc = self.init(args)
         print("Init", self.error_string(rc), self.myproc)
-        if pmix.PMIX_SUCCESS != rc:
+        if PMIX_SUCCESS != rc:
             raise Exception('PMIx_tool_init failed: ', self.error_string(rc))
-        try:
-            map = {
-                'toolconnected': server_upcalls.toolconnected,
-                'query': server_upcalls.clientquery,
-                'registerevents': server_upcalls.client_register_events,
-                'allocate': server_upcalls.allocation,
-                'sessioncontrol': server_upcalls.session_ctrl
-            }
-        except Exception as error:
-            print("FAILED TO DEFDINE MAP")
-            return
+        # check if we found the system server - we assume
+        # the system server is the DVM master controller
+        self.dvmattached = self.is_connected()
+        # set our server module entry points
+        map = {
+            'toolconnected': self.toolconnected,
+            'query': self.clientquery,
+            'registerevents': self.client_register_events,
+            'allocate': self.allocate,
+            'sessioncontrol': self.session_ctrl
+        }
         try:
             rc = self.set_server_module(map)
         except Exception as error:
             print("FAILED TO MAP")
             return
-        if pmix.PMIX_SUCCESS != rc:
+        if PMIX_SUCCESS != rc:
             raise Exception('Setting server module failed: ', self.error_string(rc))
 
     def queue_init(self):
@@ -153,32 +150,81 @@ class Scheduler(pmix.PMIxScheduler):
         except Exception as error:
             print('Unable to create FIFO instance')
 
-    def queue_names(self):
-        names = self.fifo.queue_names()
-        return names
-
     def query_allocation(self):
-        pyq = [{"keys": [pmix.PMIX_QUERY_ALLOCATION], "qualifiers": []}]
+        pyq = [{"keys": [PMIX_QUERY_ALLOCATION], "qualifiers": []}]
         rc, pyresults = self.query(pyq)
-        if rc != pmix.PMIX_SUCCESS and rc != pmix.PMIX_OPERATION_SUCCEEDED:
+        if rc != PMIX_SUCCESS and rc != PMIX_OPERATION_SUCCEEDED:
             raise Exception("QUERY ALLOCATION FAILED: ", self.error_string(rc))
         darray = pyresults[0]["value"]["array"]
         for d in darray:
-            if pmix.PMIX_NODE_INFO.decode() == d["key"]:
+            if PMIX_NODE_INFO.decode() == d["key"]:
                 n = d["value"]["array"]
                 self.allocation.append(n)
             elif (
-                pmix.PMIX_HWLOC_XML_V2.decode() == d["key"]
-                or pmix.PMIX_HWLOC_XML_V1.decode() == d["key"]
+                PMIX_HWLOC_XML_V2.decode() == d["key"]
+                or PMIX_HWLOC_XML_V1.decode() == d["key"]
             ):
                 t = d["value"]
                 self.topologies.append(t)
             else:
                 print("ALLOCATION CONTAINS UNEXPECTED DATA", d["key"])
-        print("ALLOCATION:", self.allocation)
 
+    def toolconnected(self, args:dict is not None):
+        if 'nspace' in args:
+            ns = args['nspace']
+        else:
+            ns = "dschedtool"
+
+        results = {'nspace': ns, 'rank': 0}
+        return PMIX_OPERATION_SUCCEEDED, results
+
+    def clientquery(self, args:dict is not None):
+        print("SERVER: QUERY")
+        # return a python info list of dictionaries
+        info = {}
+        results = []
+        rc = PMIX_ERR_NOT_FOUND
+        # find key we passed in to client, and
+        # if it matches return fake PSET_NAME
+        # since RM actually assigns this, we
+        # just return arbitrary name if key is
+        # found
+        find_str = 'pmix.qry.psets'
+        for q in args['queries']:
+            for k in q['keys']:
+                if k == find_str:
+                    info = {'key': find_str, 'value': 'PSET_NAME', 'val_type': PMIX_STRING}
+                    results.append(info)
+                    rc = PMIX_SUCCESS
+        return rc, results
+
+    def client_register_events(self, args:dict is not None):
+        print("CLIENT REGISTER EVENTS ", args['codes'])
+        return PMIX_OPERATION_SUCCEEDED
+
+    def allocate(self, args:dict is not None):
+        # see what they want us to allocate
+        print("ARGS", args)
+        # see what we have
+        print("RESOURCES", self.allocation)
+        results = []
+        return PMIX_ERR_NOT_SUPPORTED, results
+
+    def session_ctrl(self, args:dict is not None):
+        print("DSCHED SESSION CTRL")
+        return PMIX_ERR_NOT_SUPPORTED
+
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
 if __name__ == "__main__":
     global killer
-    killer = server_upcalls.GracefulKiller()
+    killer = GracefulKiller()
     main()
